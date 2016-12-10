1da342c5d9e377f2765f861142730ed65aa3c32a
Matthew Meyers <m.j.mey3rs@gmail.com>
2016-12-10 13:29:54 -0600

    Removing redundancies in logger statements (#6)
    * Removing redundancies in logger statements
    
    * Fixes typo
    
    * Removing module

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var MTGClient = require('mtgsdk').card;
 var Renderer = require('./renderer');
 var Serializer = require('./serializer');
-var ERROR_MESSAGE = '*No results from the AEther*. (Hint: include all punctuation in the card\'s name, e.g. "narset enlightened master" should be "narset, enlightened master")'
+var ERROR_MESSAGE = '*No results from the AEther*. (Hint: include all punctuation in the card\'s name, e.g. "narset enlightened master" should be "narset, enlightened master")';
 
 exports.handler = function(event, context) {
   console.log('Received:', event);
   var query = { name: '"' + event.text + '"'};
-  console.log('Query:', query);
   MTGClient.where(query).then(function(cards) {
-    console.log('Deserializing:', cards);
     var success = Serializer.deserialize(cards, function(result) {
       console.log('Deserialized:', result);
       var response = Renderer(result);
